8d0ff1950fabe9c3976e0c50dd08cbf393d8fe9b
Esra Yaşar <yasaresra11@icloud.com>
2024-11-23 13:44:25 +0300

    Count votes, add new metrics, move calculation code somewhere else

--- strategic_voting.py
+++ strategic_voting.py
@@ -1,25 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+from functools import partial
+import random
 import mesa
 import seaborn as sns
 import numpy as np
 import pandas as pd
-import random
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
+import pprint
 
 # List of parties. Negative ones are left-wing, positive ones are
-# right-wing. Bigger the absolute value, lesser the radicality of the
-# party.
-preferences = [-9, -7, -5, -3, -1, 0, 1, 3, 5, 7, 9]
+# right-wing. Radical parties have higher weight.
+left = [-9, -7, -5, -3, -1]
+right = [1, 3, 5, 7, 9]
+preferences = left + [0] + right
+preferences_p = left + right
+
+# Keep also names.
+names = ["E", "D", "C", "B", "A", "O", "A'", "B'", "C'", "D'", "E'"]
 
 weights = [1, 3, 5, 7, 9]
 
-#colors = [(250, 187, 187), (250, 135, 135), (252, 101, 101), (250, 52, 52), (250, 15, 15), (0, 0, 0),
-#          (15, 39, 250), (77, 95, 250), (130, 142, 250), (174, 182, 252), (214, 218, 255)]
-#colors = [(e[0] / 255.0, e[1] / 255.0, e[2] / 255.0) for e in colors]
-#cmap = ListedColormap(colors)
+colors = [(250, 187, 187), (250, 135, 135), (252, 101, 101), (250, 52, 52), (250, 15, 15), (0, 0, 0),
+          (15, 39, 250), (77, 95, 250), (130, 142, 250), (174, 182, 252), (214, 218, 255)]
+
+colors = [(e[0] / 255.0, e[1] / 255.0, e[2] / 255.0) for e in colors]
+cmap = ListedColormap(colors)
+
+def portions(n):
+    a = random.sample(range(1, n), 4) + [0, n]
+    list.sort(a)
+    return sorted([a[i + 1] - a[i] for i in range(len(a) - 1)])
 
 class Voter(mesa.Agent):
     """A voter with fixed initial preference."""
@@ -27,50 +37,99 @@ class Voter(mesa.Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.preference = random.choice(preferences)
+        #self.preference = model.voters.pop()
+        #self.preference = random.choices(preferences, left_portions + [4]
+        #                                 + right_portions)[0]
         self.next_preference = None
 
     def step(self):
-        #print(f"Hi, I am a voter, you can call me {str(self.unique_id)}. I will vote to {str(self.preference)}.")
         neighbors = self.model.grid.get_neighbors(
             self.pos,
-            moore=True,
-            include_center=False)
+            moore=False,
+            include_center=False,
+            radius=1)
         #print(neighbors)
         neighbors_prefs = [neighbor.preference for neighbor in neighbors]
         #print(sum(neighbors_prefs))
         neighbors_sum = sum(neighbors_prefs)
         if neighbors_sum == 0: # It's a tie, then I vote sincerely.
             self.next_preference = self.preference
-        elif neighbors_sum > 0: # Right-wing is winning, then I vote for the left-wing.
-            for weight in weights:
-                if neighbors_sum + (-weight) <= -4:
-                    self.next_preference = -weight
-                    #print(f"{str(self.pos)}, {str(neighbors_sum)}, {str(-weight)}")
-                    break
+            self.model.outcomes["tie"] += 1
+        elif neighbors_sum > 0:
+            # Right-wing is winning. If I am a rightist, then I vote
+            # sincerely, otherwise I vote strategically.
+            if self.preference < 0:
+                for weight in weights:
+                    if neighbors_sum + self.preference <= 0:
+                        self.next_preference = self.preference
+                        break
+                    if neighbors_sum + (-weight) <= 0:
+                        self.next_preference = -weight
+                        self.model.outcomes["left_suc_strategic"] += 1
+                        #print(f"{str(self.pos)}, {str(neighbors_sum)}, {str(-weight)}")
+                        break
+                else:
+                    #toss = random.choice([-1, 0, 1])
+                    toss = random.choice([-1])
+                    if toss == -1:
+                        self.next_preference = -9 # Left-wing will not win, but I will try my best.
+                        self.model.outcomes["left_unsuc_best_try"] += 1
+                    elif toss == 0:
+                        self.next_preference = self.preference
+                        self.model.outcomes["left_unsuc_sincere"] += 1
+                    elif toss == 1:
+                        self.next_preference = random.choice(preferences_p)
+                        self.model.outcomes["left_unsuc_random"] += 1
             else:
-                #self.next_preference = 9 # Left-wing will not win, but I will try my best.
                 self.next_preference = self.preference
-                #self.next_preference = random.choice(preferences)
-        elif neighbors_sum < 0: # Left-wing is winning, then I vote for the right-wing.
-            for weight in weights:
-                if neighbors_sum + weight >= 4:
-                    self.next_preference = weight
-                    #print(f"{str(self.pos)}, {str(neighbors_sum)}, {str(weight)}")
-                    break
+                self.model.outcomes["right_winning_sincere"] += 1
+        elif neighbors_sum < 0:
+            # Left-wing is winning. If I am a leftist, then I vote
+            # sincerely, otherwise I vote strategically.
+            if self.preference > 0:
+                for weight in weights:
+                    if neighbors_sum + self.preference >= 0:
+                        self.next_preference = self.preference
+                        break
+                    if neighbors_sum + weight >= 0:
+                        self.next_preference = weight
+                        self.model.outcomes["right_suc_strategic"] += 1
+                        #print(f"{str(self.pos)}, {str(neighbors_sum)}, {str(weight)}")
+                        break
+                else:
+                    #toss = random.choice([-1, 0, 1])
+                    toss = random.choice([-1])
+                    if toss == -1:
+                        self.next_preference = 9 # Left-wing will not win, but I will try my best.
+                        self.model.outcomes["right_unsuc_best_try"] += 1
+                    elif toss == 0:
+                        self.next_preference = self.preference
+                        self.model.outcomes["right_unsuc_sincere"] += 1
+                    elif toss == 1:
+                        self.next_preference = random.choice(preferences_p)
+                        self.model.outcomes["right_unsuc_random"] += 1
             else:
-                #self.next_preference = -9 # Right-wing will not win, but I will try my best.
                 self.next_preference = self.preference
-                #self.next_preference = random.choice(preferences)
+                self.model.outcomes["left_winning_sincere"] += 1
+
     def advance(self):
         self.preference = self.next_preference
 
-def count_preferences(model):
-    preferences = [agent.preference for agent in model.schedule.agents]
-    preferences_set = set(preferences)
-    preferences = dict([(preference, preferences.count(preference))
-                    for preference in preferences_set])
+# Construct the reporter dictionary.
+def count_preferences(model, preference):
+    prefs_list = [agent.preference for agent in model.schedule.agents]
+    prefs_set = set(preferences)
+    prefs_dict = dict([(preference, prefs_list.count(preference))
+                       for preference in prefs_set])
 
-    return preferences[5]
+    return prefs_dict[preference]
+
+model_reporters = {str(names[i]): partial(count_preferences,
+                                            preference=preference) for
+                   i, preference in enumerate(preferences)}
+
+# Add the outcomes reporter.
+model_reporters["outcomes"] = lambda m : dict(m.outcomes)
 
 class VotingModel(mesa.Model):
     """Model with some voters possibly voting strategically."""
@@ -78,52 +137,51 @@ class VotingModel(mesa.Model):
     def __init__(self, width):
         super().__init__()
         self.num_voters = width * width
+
         self.grid = mesa.space.SingleGrid(width, width, True)
         self.schedule = mesa.time.SimultaneousActivation(self)
 
+        # Keep count of sincere and strategic votes.
+        self.outcomes = {
+                "tie" : 0,
+                "left_suc_strategic" : 0,
+                "left_unsuc_best_try" : 0,
+                "left_unsuc_sincere" : 0,
+                "left_unsuc_random" : 0,
+                "left_winning_sincere" : 0,
+                "right_suc_strategic" : 0,
+                "right_unsuc_best_try" : 0,
+                "right_unsuc_sincere" : 0,
+                "right_unsuc_random" : 0,
+                "right_winning_sincere" : 0,
+                }
+
+        # Initiate portions of the parties
+        #left_portions = sorted(portions(48), reverse=True)
+        #right_portions = portions(48)
+
+        #print(left_portions, right_portions)
+
+        #self.voter_portions = left_portions + [4] + right_portions
+        #self.voters = []
+        #for index, i in enumerate(self.voter_portions):
+        #    # Assume width is a multiple of 10.
+        #    for _ in range(i * self.num_voters // 100):
+        #        self.voters.append(preferences[index])
+
         # Create voters.
         for i in range(self.num_voters):
-            a = Voter(i, self)
-            self.schedule.add(a)
+            voter = Voter(i, self)
+            self.schedule.add(voter)
 
-            # Add the agent to a random grid cell
-            x = a.unique_id % width
-            y = a.unique_id // width
-            self.grid.place_agent(a, (x, y))
+            # Place the agent to a random grid cell
+            self.grid.place_agent(voter, random.choice(list(self.grid.empties)))
 
-        self.datacollector = mesa.DataCollector(
-            model_reporters={"Votes": count_preferences},
-            agent_reporters={"Preference": "preference"}
-        )
+        self.datacollector = mesa.DataCollector(model_reporters=model_reporters)
 
     def step(self):
         self.datacollector.collect(self)
+        # Reset outcomes counters.
+        for key in self.outcomes:
+            self.outcomes[key] = 0
         self.schedule.step()
-
-#elections = []
-#
-#votes = np.zeros((model.grid.width, model.grid.height))
-#
-#for voter, (x, y) in model.grid.coord_iter():
-#    vote = voter.preference
-#    votes[x][y] = vote
-#
-#votes = votes.T
-#elections.append(votes)
-#
-#for i in range(150):
-#    model.step()
-#
-#    votes = np.zeros((model.grid.width, model.grid.height))
-#
-#    for voter, (x, y) in model.grid.coord_iter():
-#        vote = voter.preference
-#        votes[x][y] = vote
-#
-#    votes = votes.T
-#    elections.append(votes)
-#
-#for year, election in enumerate(elections):
-#    g = sns.heatmap(election, cmap=cmap, annot=False, cbar=False, square=True)
-#    plt.savefig(f"{str(year)}.png")
-#    plt.clf()
